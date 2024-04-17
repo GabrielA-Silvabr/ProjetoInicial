4963f0bff01cbd0c6284a1bc67f321f0c66327a7
Gabriel Anunciação <gabrielanunciacao98@gmail.com>
2024-04-17 16:07:34 -0300

    Primeiro projeto

---
+++ app.js
@@ -0,0 +1,10 @@
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+
+function verificarChute() {
+    console.log ('o botão foi clicado)')
+}
